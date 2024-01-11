a804d53083cdca5e7642fb4066728637997ff1af
rodgersxy <rodgersny99@gmail.com>
2024-01-11 14:59:50 +0300

    MySQL

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -72,3 +72,28 @@ def get_db() -> connection.MySQLConnection:
         user=username, password=password, host=db_host, database=db_name
     )
     return connector
+
+
+def main() -> None:
+    """
+    main function
+    """
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+
+    headers = [field[0] for field in cursor.description]
+    logger = get_logger()
+
+    for row in cursor:
+        info_answer = ''
+        for f, p in zip(row, headers):
+            info_answer += f'{p}={(f)}; '
+        logger.info(info_answer)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    main()
